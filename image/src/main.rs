a7093820e849948e7e9b54e2daf9a512a4d3a483
Arthur Damasceno <damascenoarthur653@gmail.com>
2024-05-02 18:10:34 -0300

    feat: image cli

--- image/src/main.rs
+++ image/src/main.rs
@@ -1,17 +1,27 @@
-use std::{env, io::Result};
+use std::{env, error::Error};
 
 use image::Image;
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn Error>> {
+    let operation = env::var("OPERATION")
+        .unwrap_or("L".into())
+        .chars()
+        .next()
+        .expect("Invalid operation");
     let path_open = env::var("PATH_OPEN").unwrap_or("./image/data/cat.pgm".into());
     let path_save = env::var("PATH_SAVE").unwrap_or("image.pgm".into());
-    let treshold = env::var("THRESHOLD")
-        .map(|value| value.parse().unwrap())
-        .unwrap_or(127);
+    let thresh = env::var("THRESH").unwrap_or("127".into()).parse()?;
 
     let image = Image::open(&path_open)?;
 
-    let threshold = image.threshold(treshold);
+    let processed = match operation {
+        'M' => image.mean_filter(),
+        'L' => image.laplacian_filter(),
+        'T' => image.threshold(thresh),
+        _ => panic!("Invalid operation"),
+    };
 
-    threshold.save(&path_save)
+    processed.save(&path_save)?;
+
+    Ok(())
 }
